07be171d2cb6d3811988b7f75cc416b82e977f9b
Le Luong <leluong.dev@gmail.com>
2024-04-23 15:49:44 +0700

    edit

--- CONFIG.js
+++ CONFIG.js
@@ -2,9 +2,9 @@ const CONFIGDATA = {
   titleHeader: "Em à, Anh có điều muốn nói.",
   descriptionHeader:
     "Hôm nay là ngày đặc biệt gì Em nhớ không. Là ngày tròn 1 tháng chúng mình quen nhau đấy hehee. (23/03)",
-  buttonYes: "Yêu anh Lương <3",
+  buttonYes: "Yêu anh <3",
   buttonNo: "Không :D",
   titleModar: "Cảm ơn Em !",
   descriptionModar:
-    "Cảm ơn Em đã cho Anh cơ hội để có thể ở bên cạnh, che chở và yêu thương Em. </br> Anh biết mình không được lãng mạn, cũng chẳng khéo ăn nói nhiều lúc làm Em buồn nhưng Anh luôn cố gắng để tốt hơn mỗi ngày, yêu thương Em bằng hết những gì Anh có thể. </br> Hôm nay là một ngày đặc biệt, Anh chỉ muốn nói Em là một cô gái tuyệt vời, Anh thật hạnh phúc khi có Em bên cạnh, Anh yêu Em nhiều ! mình cùng nhau cố gắng Em nhé hehe.</br> Love Nga <3 </br> Luong Le <3",
+    "Cảm ơn Em đã cho Anh cơ hội để có thể ở bên cạnh, che chở và yêu thương Em. </br> Anh biết mình không được lãng mạn, cũng chẳng khéo ăn nói nhiều lúc làm Em buồn nhưng Anh luôn cố gắng để tốt hơn mỗi ngày, yêu thương Em bằng hết những gì Anh có thể. </br> Hôm nay là một ngày đặc biệt, Anh chỉ muốn nói Em là một cô gái tuyệt vời, Anh thật hạnh phúc khi có Em bên cạnh, Anh yêu Em nhiều ! mình cùng nhau cố gắng Em nhé hehe.</br> Luong Love Nga <3",
 };
